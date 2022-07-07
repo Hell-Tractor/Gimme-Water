3100f382300a22302634b5b10c92d3a4f8f57d2d
Hell-Tractor <1104840448@qq.com>
2022-07-07 17:38:57 +0800

    Add audio playing components

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    private static GameManager _instance;
+    public static GameManager Instance { get { return _instance; } }
+
+    public AudioSource BGMSource;
+    public AudioSource SFXSource;
+
+    private void Awake() {
+        if (_instance != null && _instance != this) {
+            Destroy(this.gameObject);
+            Debug.LogWarning("Duplicate GameManager detected. Destroying...");
+        } else {
+            _instance = this;
+        }
+    }
+}
