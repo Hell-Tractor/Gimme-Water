8542a192d486a67cbac76cde0ab58a932ff40f91
Hell-Tractor <1104840448@qq.com>
2022-07-07 17:34:32 +0800

    Modified RadarTarget to support remote

--- Assets/Scripts/Minimap/RadarTarget.cs
+++ Assets/Scripts/Minimap/RadarTarget.cs
@@ -1,9 +1,24 @@
 using UnityEngine;
 using UnityEngine.UI;
+using Mirror;
 
-public class RadarTarget : MonoBehaviour {
-    public Sprite Sprite;
-    public Color Color;
+public class RadarTarget : NetworkBehaviour {
+    public Sprite LocalSprite;
+    public Sprite RemoteSprite;
+    public Color LocalColor;
+    public Color RemoteColor;
+
+    public Color Color {
+        get {
+            return isLocalPlayer ? LocalColor : RemoteColor;
+        }
+    }
+
+    public Sprite Sprite {
+        get {
+            return isLocalPlayer ? LocalSprite : RemoteSprite;
+        }
+    }
 
     [HideInInspector]
     public Image ImageOnRadar = null;
